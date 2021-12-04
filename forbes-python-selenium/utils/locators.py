3779d798c704c4813949848f4457cba3e5a615d0
Md. Rifat Islam <xxxxxxrifatxxxxxx@gmail.com>
2021-12-05 01:38:23 +0600

    4 Page Locators Done

--- forbes-python-selenium/utils/locators.py
+++ forbes-python-selenium/utils/locators.py
@@ -2,84 +2,184 @@ from selenium.webdriver.common.by import By
 
 
 class HomePageLocators(object):
-    POST_YOUR_ADD_BUTTON = (By.XPATH, "/html/body/nav/div/ul[3]/li[5]/a")
-    LOCATION_1 = (By.CLASS_NAME, "locations-1")
-    LOCATION_2 = (By.CLASS_NAME, "locations-2")
-    LOGIN_BUTTON = (By.XPATH, "/html/body/nav/div/ul[3]/li[4]/a/span")
-    MY_ACCOUNT = (By.XPATH, "/html/body/nav/div/ul[3]/li[3]")
-    FAQ = (By.LINK_TEXT, "FAQ")
-    COPYRIGHT_TEXT = (By.XPATH, "/html/body/div[3]/div[2]/div[1]")
-    ALL_ADS_LINK = (By.LINK_TEXT, "All ads")
-
-
-class CityPageLocators(object):
-    PRICES_CLASS = (By.CLASS_NAME, "price--3SnqI")
-
-
-class ProductPageLocators(object):
-    POSTED_ON_CLASS = (By.CLASS_NAME, "sub-title--37mkY")
-    DESCRIPTION_CLASS = (By.CLASS_NAME, "description--1nRbz")
-    CALL_BUTTON = (By.CLASS_NAME, "call-button--3uvWj")
-    PHONE_NUMBER = (By.CLASS_NAME, "phone-numbers--2COKR")
-
-
-class AdsPageLocators(object):
-    COPYRIGHT_TEXT = (
-        By.XPATH,
-        "/html/body/div[1]/div/div[1]/div[6]/div/div/div/div[3]/div[1]",
-    )
-    LINKS_CLASS = (By.CLASS_NAME, "list-links--odRYW")
-    ALL_ADS_TEXT = (By.XPATH, "//div[contains(text(),'All ads in Bangladesh')]")
-    URGENT_CHECKBOX = (
-        By.XPATH,
-        "//body/div[@id='app-container']/div[@id='app-wrapper']/div[1]/div[3]/div[1]/div[2]/div[2]/div[1]/div[2]/div[2]/div[1]/label[1]/span[1]",
-    )
-    MOBILE_CHECKBOX = (By.XPATH, "//span[contains(text(),'Mobiles')]")
-    MOBILE_TEXT = (
-        By.XPATH,
-        "//h1[contains(text(),'Mobiles Phones and Accessories for Sale in Banglad')]",
+    SUBSCRIBE = (
+        By.CSS_SELECTOR,
+        "body > div.main-content.main-content--universal-header > header > nav > div.header__right > div > a",
     )
 
-
-class LoginPageLocators(object):
-    CONTINUE_WITH_EMAIL_BUTTON = (
-        By.XPATH,
-        "/html/body/div[1]/div/div[2]/div[3]/div[2]/div[2]/div/div[3]/button",
-    )
-    EMAIL_TEXTBOX = (By.ID, "input_email")
-    PASSWORD_TEXTBOX = (By.ID, "input_password")
-    SUBMIT_BUTTON = (
-        By.XPATH,
-        "/html/body/div[1]/div/div[2]/div[3]/div[2]/div[2]/div/form/div[2]/div/button",
-    )
-    LOGIN_FAILED_TEXT = (
-        By.XPATH,
-        "/html[1]/body[1]/div[1]/div[1]/div[2]/div[3]/div[2]/div[2]/div[1]/div[1]/span[1]",
+    SIGN_IN = (
+        By.CSS_SELECTOR,
+        "body > div.main-content.main-content--universal-header > header > nav > div.header__right > div > div > button",
     )
 
+    SEARCH_BUTTON = (
+        By.CSS_SELECTOR,
+        "body > div.main-content.main-content--universal-header > header > nav > div.header__right > button",
+    )
 
-class DashboardPageLocators(object):
-    SETTINGS = (By.LINK_TEXT, "Settings")
+    SEARCH_INPUT = (
+        By.CSS_SELECTOR,
+        "body > div.main-content.main-content--universal-header > div.search-modal > form > input",
+    )
 
+    EXPLORE = (
+        By.CSS_SELECTOR,
+        "body > div.main-content.main-content--universal-header > header > nav > div.header__left > button.icon--hamburger",
+    )
 
-class SettingsPageLocators(object):
-    OLD_PASSWORD_FIELD = (By.ID, "old")
-    NEW_PASSWORD_FIELD = (By.ID, "password")
-    CONFIRM_NEW_PASSWORD_FIELD = (By.ID, "confirm")
-    CHANGE_PASSWORD_BUTTON = (
-        By.XPATH,
-        "/html/body/div[2]/div/div[1]/div/div/div[2]/div[1]/form[2]/div[4]/button",
+    BILLIONAIRE = (
+        By.CSS_SELECTOR,
+        "body > div.main-content.main-content--universal-header > header > nav > div.header__channels--wrapper > ul > li.header__channel.header__color--centennial-silver.header__hoverable",
+    )
+
+    WORLDS_BILLIONAIRE = (
+        By.CSS_SELECTOR,
+        "body > div.main-content.main-content--universal-header > header > nav > div.header__channels--wrapper > ul > li.header__channel.header__color--centennial-silver.header__hoverable > div.header__subnav > ul > li:nth-child(2) > a",
+    )
+
+    FORBES_400 = (
+        By.CSS_SELECTOR,
+        "body > div.main-content.main-content--universal-header > header > nav > div.header__channels--wrapper > ul > li.header__channel.header__color--centennial-silver.header__hoverable > div.header__subnav > ul > li:nth-child(3) > a'",
+    )
+
+    INNOVATION = (
+        By.CSS_SELECTOR,
+        "body > div.main-content.main-content--universal-header > header > nav > div.header__channels--wrapper > ul > li.header__channel.header__color--diamondring-blue.header__hoverable > a",
+    )
+
+    FIVE_G = (
+        By.CSS_SELECTOR,
+        "body > div.main-content.main-content--universal-header > header > nav > div.header__channels--wrapper > ul > li.header__channel.header__color--diamondring-blue.header__hoverable > div.header__subnav > ul > li:nth-child(2) > a",
+    )
+
+    AI = (
+        By.CSS_SELECTOR,
+        "body > div.main-content.main-content--universal-header > header > nav > div.header__channels--wrapper > ul > li.header__channel.header__color--diamondring-blue.header__hoverable > div.header__subnav > ul > li:nth-child(3) > a",
+    )
+
+    FORBES_LIVE_EVENTS = (
+        By.CSS_SELECTOR,
+        "body > div.main-content.main-content--universal-header > footer > div.footer--desktop.fs-content > div.footer__content > div:nth-child(1) > div:nth-child(2) > a",
+    )
+
+    FORBES_BILLIONAIRES_PORTFOLIO = (
+        By.CSS_SELECTOR,
+        "body > div.main-content.main-content--universal-header > footer > div.footer--desktop.fs-content > div.footer__content > div:nth-child(1) > div:nth-child(4) > a:nth-child(1)",
+    )
+
+    ADVERTISE = (
+        By.CSS_SELECTOR,
+        "body > div.main-content.main-content--universal-header > footer > div.footer--desktop.fs-content > div.footer__content > div:nth-child(2) > div:nth-child(4) > a:nth-child(1)",
+    )
+
+    FORBES_ISRAEL = (
+        By.CSS_SELECTOR,
+        "body > div.main-content.main-content--universal-header > footer > div.footer--desktop.fs-content > div.footer__content > div.footer__column--right > div.footer__bottomright-promo > div > div:nth-child(2) > a:nth-child(1)",
+    )
+
+    FORBES_JAPAN = (
+        By.CSS_SELECTOR,
+        "body > div.main-content.main-content--universal-header > footer > div.footer--desktop.fs-content > div.footer__content > div.footer__column--right > div.footer__bottomright-promo > div > div:nth-child(2) > a:nth-child(3)",
     )
 
 
-class FAQPageLocators(object):
-    FAQ = (By.CLASS_NAME, "faq")
-    FAQS = (By.TAG_NAME, "dt")
-    TEXT = (By.TAG_NAME, "dd")
+class AdvertisePageLocators(object):
+    US_EUROPE = (
+        By.CSS_SELECTOR,
+        "body > main > div.media-hero-section > div > div.tabs-widget > div > ul > li:nth-child(1) > a",
+    )
+
+    US_EUROPE_1ST_POST = (
+        By.CSS_SELECTOR,
+        "#\\32 021_media_kit > div > div > div.col-md-5.media-kit-content > div > a",
+    )
+
+    ASIA_PACIFIC = (
+        By.CSS_SELECTOR,
+        "body > main > div.media-hero-section > div > div.tabs-widget > div > ul > li:nth-child(2) > a",
+    )
+
+    ASIA_PACIFIC_POST = (
+        By.CSS_SELECTOR,
+        "#\\32 021_forbes_asia_media_kit > div > div > div.col-md-5.media-kit-content > div > a",
+    )
 
 
-class Page404Locators(object):
-    TEXT = (
-        By.XPATH,
-        "/html[1]/body[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/h1[1]",
+class BillionairesPageLocators(object):
+
+    FACEBOOK_SHARE = (
+        By.CSS_SELECTOR,
+        "#gatsby-focus-wrapper > div > div > div.title.has-left-rail > div.left-rail > div:nth-child(3) > div.social > div > div:nth-child(1)",
+    )
+
+    TWITTER_SHARE = (
+        By.CSS_SELECTOR,
+        "#gatsby-focus-wrapper > div > div > div.title.has-left-rail > div.left-rail > div:nth-child(3) > div.social > div > div:nth-child(2)",
+    )
+
+    LINKDIN_SHARE = (
+        By.CSS_SELECTOR,
+        "#gatsby-focus-wrapper > div > div > div.title.has-left-rail > div.left-rail > div:nth-child(3) > div.social > div > div:nth-child(3)",
+    )
+
+    MAIL = (
+        By.CSS_SELECTOR,
+        "#gatsby-focus-wrapper > div > div > div.title.has-left-rail > div.left-rail > div:nth-child(3) > div.social > div > div:nth-child(4)",
+    )
+
+    SEARCH = (
+        By.CSS_SELECTOR,
+        "#gatsby-focus-wrapper > div > header > div > a.header__search",
+    )
+
+    SEARCH_INPUT = (
+        By.CSS_SELECTOR,
+        "body > div.main-content.main-content--overflow-visible.main-content--universal-header > main > div.search-content-wrapper > div.search-content.main-content__left-col > div.search-box > input",
+    )
+
+    ELON_MUSK = (
+        By.CSS_SELECTOR,
+        "#elon-musk > div.personName",
+    )
+
+    ELON_MUSK_PROFILE = (
+        By.CSS_SELECTOR,
+        "#gatsby-focus-wrapper > div > div > div.table-block > div.table-block-wrapper > div.table > div.table-body > div:nth-child(1) > div.expanded-content > div.person-bio > div.bio-container > div.bio-button-container > a",
+    )
+
+
+class BillionairesPortfolioPageLocators(object):
+    EDITION = (
+        By.CSS_SELECTOR,
+        "#row-0 > div.channel__header > div.edition-dropdown > button",
+    )
+
+    US = (
+        By.CSS_SELECTOR,
+        "#row-0 > div.channel__header > div.edition-dropdown.edition-dropdown--open > ul > li:nth-child(1) > a",
+    )
+
+    INTEL_SAYS_POST = (
+        By.CSS_SELECTOR,
+        "#row-0 > div.channel__content > div.card.card--large.csf-block > div.card--large__title > a",
+    )
+
+    ASIA = (
+        By.CSS_SELECTOR,
+        "#row-0 > div.channel__header > div.edition-dropdown.edition-dropdown--open > ul > li:nth-child(2)",
+    )
+
+    AC_VENTURES_POST = (
+        By.CSS_SELECTOR,
+        "#row-0 > div.channel__content > div.channel__columns > div:nth-child(1) > div > a.headlink",
+    )
+
+    EUROPE = (
+        By.CSS_SELECTOR,
+        "#row-0 > div.channel__header > div.edition-dropdown.edition-dropdown--open > ul > li:nth-child(3)",
+    )
+
+    HALLOFFRAMEPOST = (
+        By.CSS_SELECTOR,
+        "#row-0 > div.channel__content > div.channel__columns > div:nth-child(1) > div > a.headlink",
     )
